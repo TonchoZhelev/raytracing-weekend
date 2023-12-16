b0e48f9843b32a4bc64cd63cf307c3c1abcdd566
Toncho Zhelev <toncho.zhelev@gmail.com>
2023-12-16 18:01:21 +0200

    Positioning and Orienting the Camera (12.2)

--- src/camera.hpp
+++ src/camera.hpp
@@ -13,10 +13,15 @@
 
 class Camera {
     public:
-        float aspect_ratio = 1.0f; // Ratio of image width over height 
-        int   image_width = 100;
-        int   samples_per_pixel = 10;
-        int   max_depth = 10; // Maximum number of ray bounces into scene
+        float aspect_ratio      = 1.0f; // Ratio of image width over height 
+        int   image_width       = 100;  // Rendered image width in pixel count
+        int   samples_per_pixel = 10;   // Count of random samples for each pixel
+        int   max_depth         = 10;   // Maximum number of ray bounces into scene
+
+        float  vfov = 90;                    // Vertical view angle (field of view)
+        point3 look_from = point3(0, 0, -1); // Point camera is looking from
+        point3 look_at   = point3(0,0,0);    // Point camera is looking at
+        vec3   vup       = vec3(0,1,0);      // Camera-relative "up" direction
 
         void render(const IHittable& world) {
             initialize();
@@ -46,21 +51,29 @@ class Camera {
         point3 pixel00_loc;    // Location of pixel 0, 0
         vec3   pixel_delta_u;  // Offset to pixel to the right
         vec3   pixel_delta_v;  // Offset to pixel below
+        vec3   u, v, w; // Camera frame basis vectors
 
         void initialize() {
             image_height = static_cast<int>(image_width / aspect_ratio);
             image_height = (image_height < 1) ? 1 : image_height;
 
-            center = point3(0, 0, 0);
+            center = look_from;
 
             // Determine viewport dimensions.
-            float focal_length = 1.0f;
-            float viewport_height = 2.0f;
+            float focal_length = (look_from - look_at).length();
+            float theta = degrees_to_radians(vfov);
+            float h = tanf(theta/2);
+            float viewport_height = 2 * h * focal_length;
             float viewport_width = viewport_height * (static_cast<float>(image_width)/image_height);
 
+            // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+            w = (look_from - look_at).unit();
+            u = vup.cross(w).unit();
+            v = w.cross(u);
+
             // Calculate the vectors across the horizontal and down the vertical viewport edges
-            vec3 viewport_u = vec3(viewport_width, 0, 0);
-            vec3 viewport_v = vec3(0, -viewport_height, 0);
+            vec3 viewport_u = viewport_width * u;   // Vector across viewport horizontal edge
+            vec3 viewport_v = viewport_height * -v; // Vector down viewport vertical edge
 
 
             // Calculate the horizontal and vertical delta vectors from pixel to pixel.
@@ -68,7 +81,7 @@ class Camera {
             pixel_delta_v = viewport_v / image_height;
 
             // Calculate the location of the upper left pixel.
-            vec3 viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+            vec3 viewport_upper_left = center - (focal_length * w) - viewport_u/2 - viewport_v/2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
         }
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,15 @@ int main(int const argc, char const *const *const argv) {
     // World
     HittableList world;
 
+    float R = cosf(pi/4);
+
     auto material_ground = std::make_shared<Lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = std::make_shared<Lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = std::make_shared<Dielectric>(1.5f);
     auto material_right  = std::make_shared<Metal>(color(0.8, 0.6, 0.2), 0.0);
+
+    // auto material_left  = std::make_shared<Lambertian>(color(0,0,1));
+    // auto material_right = std::make_shared<Lambertian>(color(1,0,0));
 
     world.add(std::make_shared<Sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(std::make_shared<Sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
@@ -19,12 +24,20 @@ int main(int const argc, char const *const *const argv) {
     world.add(std::make_shared<Sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
     world.add(std::make_shared<Sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
+    // world.add(std::make_shared<Sphere>(point3( -R, 0.0, -1.0), R, material_left));
+    // world.add(std::make_shared<Sphere>(point3(  R, 0.0, -1.0), R, material_right));
+
     Camera cam;
 
-    cam.aspect_ratio = 16.0f / 9.0f;
-    cam.image_width = 400;
+    cam.aspect_ratio      = 16.0f / 9.0f;
+    cam.image_width       = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth = 50;
+    cam.max_depth         = 50;
+
+    cam.vfov      = 90;
+    cam.look_from = point3(-2, 2, 1);
+    cam.look_at   = point3(0, 0, -1);
+    cam.vup       = vec3(0, 1, 0);
 
     cam.render(world);
 
