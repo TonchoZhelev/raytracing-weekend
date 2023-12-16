acc39ca269035158c3d16a135cfd4c87cca368d3
Toncho Zhelev <toncho.zhelev@gmail.com>
2023-12-16 17:35:36 +0200

    Schlick Approximation (11.4)

--- src/material.hpp
+++ src/material.hpp
@@ -2,7 +2,9 @@
 #define MATERIAL_H
 
 #include "ray.hpp"
+#include "rtweekend.hpp"
 #include "vec3.hpp"
+#include <cmath>
 
 struct HitRecord;
 
@@ -39,16 +41,57 @@ class Lambertian : public IMaterial {
 class Metal : public IMaterial {
     private:
         color _albedo;
+        double _fuzz;
 
     public:
-        Metal(const color &albedo) : _albedo(albedo) {}
+        Metal(const color &albedo, double fuzz) 
+            : _albedo(albedo), _fuzz(fuzz < 1 ? fuzz : 1) {}
 
         bool
         scatter(const ray &r_in, const HitRecord &rec, color &attenuation, ray &scattered) const override {
             vec3 reflected = reflect(r_in.direction().unit(), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + _fuzz * random_unit_vector());
             attenuation = _albedo;
+            return (scattered.direction().dot(rec.normal) > 0);
+        }
+};
+
+class Dielectric : public IMaterial {
+    private:
+        double _index_of_refraction;
+    public:
+        Dielectric(float index_of_refraction) 
+            : _index_of_refraction(index_of_refraction) {}
+
+        bool
+        scatter(const ray &r_in, const HitRecord &rec, color &attenuation, ray &scattered) const override {
+            attenuation = color(1.f, 1.f, 1.f);
+            float refraction_ratio = rec.front_face ? (1.0/_index_of_refraction) : _index_of_refraction;
+
+            vec3 unit_direction = r_in.direction().unit();
+            float cos_theta = fminf((-unit_direction).dot(rec.normal), 1.0f);
+            float sin_theta = sqrtf(1.f - cos_theta * cos_theta);
+
+            bool cannot_refract = refraction_ratio * sin_theta > 1.f;
+            bool has_to_reflect = reflectance(cos_theta, refraction_ratio) > random_float();
+            vec3 direction;
+
+            if (cannot_refract || has_to_reflect) {
+                direction = reflect(unit_direction, rec.normal);
+            } else {
+                direction = refract(unit_direction, rec.normal, refraction_ratio);
+            }
+
+            scattered = ray(rec.p, direction);
             return true;
+        }
+
+    private:
+        static float reflectance(float cosine, float ref_idx) {
+            // Use Schlick's approximation for reflectance.
+            float r0 = (1 - ref_idx) / (1 + ref_idx);
+            r0 = r0*r0;
+            return r0 + (1 - r0) * powf((1 - cosine), 5);
         }
 };
 
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -150,4 +150,11 @@ inline vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * v.dot(n) * n;
 }
 
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    float cos_theta = fminf((-uv).dot(n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -sqrtf(fabsf(1.0f - r_out_perp.lengthsq())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 #endif // VEC3_H
